2adad3cfaabc50ce828a716aa3709879cccaceef
etson14 <etsontavares14@gmail.com>
2018-05-22 16:19:35 +0200

    menu

--- src/loja_m_construcao/Loja_M_Construcao.java
+++ src/loja_m_construcao/Loja_M_Construcao.java
@@ -5,17 +5,127 @@
  */
 package loja_m_construcao;
 
-/**
- *
- * @author Edson Tavares
- */
+import java.util.Scanner;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 public class Loja_M_Construcao {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        int cod,opc,pp;
+        
+        Scanner ler =new Scanner(System.in);
+        JFrame frame =new JFrame("ok");
+     
+        
+         
+    System.out.println("*******************************************************");
+    System.out.println("*******************************************************");
+    System.out.println("**                                                   **");
+    System.out.println("**         LOJA DE MATERIAL DE CONSTRUÇÃO            **");
+    System.out.println("**                                                   **");
+    System.out.println("*******************************************************");
+    System.out.println("*******************************************************");
+    
+    
+    
+    System.out.print("Codigo:");
+    cod=ler.nextInt();
+    System.out.print("Palavra-Passe:");
+    pp=ler.nextInt();
+    
+    if(cod!=1234 || pp!=1234){
+        JOptionPane.showMessageDialog(frame,"Codigo ou Palavra-Passe Incorreto");
+    }
+    else 
+    
+    {
+        
+    System.out.println("\n\n");
+    System.out.println("*******************************************************");
+    System.out.println("*******************************************************");
+    System.out.println("**                                                   **");
+    System.out.println("**     BENVINDO A LOJA DE MATERIAL DE CONSTRUÇÃO     **");
+    System.out.println("**                                                   **");
+    System.out.println("*******************************************************");
+    System.out.println("*******************************************************");
+    System.out.println("**                                                   **");
+    System.out.println("**                                                   **");
+    System.out.println("**               1->Cliente                          **");
+    System.out.println("**               2->Endereço                         **");
+    System.out.println("**               3->Fornecedor                       **");
+    System.out.println("**               4->Funcionario                      **");
+    System.out.println("**               5->Material                         **");
+    System.out.println("**               6->Pedido                           **");
+    System.out.println("**               7->Pessoa                           **");
+    System.out.println("**               8->Venda                            **");
+    System.out.println("**                                                   **");
+    System.out.println("**               0-SAIR                              **");
+    System.out.println("**                                                   **");
+    System.out.println("*******************************************************");
+    System.out.println("*******************************************************");
+    
+    System.out.println("Escolha uma Opcao:");
+    opc=ler.nextInt();
+        
+    
+    
+    
+    
+    switch(opc){
+        
+        case 1:{
+            
+        break;}
+        
+        case 2:{
+            
+        break;}
+        
+        case 3:{
+            
+        break;}
+        
+        case 4:{
+            
+        break; }
+        
+        case 5:{
+            
+        break; }
+        
+        
+        case 6:{
+            
+            
+        }
+        
+        case 7:{
+        
+        break;    
+        }
+        
+        
+        case 8:{
+        
+        break;    
+        }
+        
+        case 0:{
+            System.exit(0);
+            
+         break;
+        }
+        
+        
+        default:System.out.println("Opcão Incorreta");
+        
+    }
+        
+    }
+   
     }
     
-}
+    }
+    
+
